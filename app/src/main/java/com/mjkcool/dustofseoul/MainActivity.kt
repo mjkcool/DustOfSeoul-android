02051f508bab646bb480e4b5f67e334e645d43af
s2019w05 <s2019w05@e-mirim.hs.kr>
2021-07-09 18:06:43 +0900

    Set logo image and new design to spinner in main / Define some of variables in main.

--- app/src/main/java/com/mjkcool/dustofseoul/MainActivity.kt
+++ app/src/main/java/com/mjkcool/dustofseoul/MainActivity.kt
@@ -2,17 +2,59 @@ package com.mjkcool.dustofseoul
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    //서울시 구 이름 리스트
+    val guArray: Array<String> = resources.getStringArray(R.array.location_gu_array)
+
+
+    //XML VIEW COMPONENTS
+    lateinit var nowTimeView: TextView //현재 시간
+    lateinit var nowLocationView: TextView //현재 위치
+    lateinit var conPm10View: TextView //미세먼지 농도
+    lateinit var conPm25View: TextView //초미세먼지 농도
+    lateinit var statPm10View: TextView //미세먼지 상태
+    lateinit var statPm25View: TextView //초미세먼지 상태
+
+    val STAT_LEVEL = mapOf(0 to "GOOD", 30 to "NORMAL", 80 to "BAD", 150 to "WORST")
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val gu = arrayOf("강남구", "강동구", "강북구", "강서구", "관악구", "광진구", "구로구", "금천구", "노원구",
-            "도봉구", "동대문구", "동작구", "마포구", "서대문구", "서초구", "성동구", "성북구", "송파구", "양천구",
-            "영등포구", "용산구", "은평구", "종로구", "중구", "중랑구")
+        //Log.d("구 리스트", guArray[0])
 
-        
+        val spinner: Spinner = findViewById(R.id.gu_spinner)
+
+        ArrayAdapter.createFromResource(this,
+            R.array.location_gu_array,
+            R.layout.spinner_view
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+        }
+
+
 
     }
+
 }
