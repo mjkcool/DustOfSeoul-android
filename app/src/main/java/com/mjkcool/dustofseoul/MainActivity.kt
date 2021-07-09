097ecd2e5eb5220e7a38fd4d5960353b3fd2d6a4
s2019w05 <s2019w05@e-mirim.hs.kr>
2021-07-09 18:26:47 +0900

    Init variables in main and add sync onclick+timeset

--- app/src/main/java/com/mjkcool/dustofseoul/MainActivity.kt
+++ app/src/main/java/com/mjkcool/dustofseoul/MainActivity.kt
@@ -1,36 +1,57 @@
 package com.mjkcool.dustofseoul
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
+import androidx.annotation.RequiresApi
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class MainActivity : AppCompatActivity() {
+
+    private val STAT_LEVEL = mapOf(0 to "GOOD", 30 to "NORMAL", 80 to "BAD", 150 to "WORST")
     //서울시 구 이름 리스트
-    val guArray: Array<String> = resources.getStringArray(R.array.location_gu_array)
+    private val GU_NAMES: Array<String> = resources.getStringArray(R.array.location_gu_array)
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private val DatetimeFormatter = DateTimeFormatter.ofPattern("MM월 dd일 h:mm")
 
 
     //XML VIEW COMPONENTS
-    lateinit var nowTimeView: TextView //현재 시간
-    lateinit var nowLocationView: TextView //현재 위치
-    lateinit var conPm10View: TextView //미세먼지 농도
-    lateinit var conPm25View: TextView //초미세먼지 농도
-    lateinit var statPm10View: TextView //미세먼지 상태
-    lateinit var statPm25View: TextView //초미세먼지 상태
+    private lateinit var nowTimeView: TextView //현재 시간
+    private lateinit var nowLocationView: TextView //현재 위치
+    private lateinit var conPm10View: TextView //미세먼지 농도
+    private lateinit var conPm25View: TextView //초미세먼지 농도
+    private lateinit var statPm10View: TextView //미세먼지 상태
+    private lateinit var statPm25View: TextView //초미세먼지 상태
 
-    val STAT_LEVEL = mapOf(0 to "GOOD", 30 to "NORMAL", 80 to "BAD", 150 to "WORST")
+    private lateinit var syncBtn: ImageButton
 
 
 
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         //Log.d("구 리스트", guArray[0])
+
+        //Initialize XML components
+        nowTimeView = findViewById(R.id.nowtime_textview)
+        nowLocationView = findViewById(R.id.nowlocation_textview)
+        conPm10View = findViewById(R.id.pm10_con)
+        conPm25View = findViewById(R.id.pm25_con)
+        statPm10View = findViewById(R.id.pm10_stat)
+        statPm25View = findViewById(R.id.pm25_stat)
+        syncBtn = findViewById(R.id.sync_btn)
+
+        syncBtn.setOnClickListener { sync() }
+
+
 
         val spinner: Spinner = findViewById(R.id.gu_spinner)
 
@@ -55,6 +76,12 @@ class MainActivity : AppCompatActivity() {
 
 
 
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun sync(){
+        var now = LocalDateTime.now()
+        nowTimeView.text = now.format(DatetimeFormatter)
     }
 
 }
